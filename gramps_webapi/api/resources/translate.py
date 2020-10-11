c81cef4d4c1081af2a03ac4e21a7782e3f6faf78
Christopher Horn <cdhorn@embarqmail.com>
2020-10-11 13:49:49 -0400

    Refactor translate endpoint for consistency

--- gramps_webapi/api/resources/translate.py
+++ gramps_webapi/api/resources/translate.py
@@ -13,26 +13,29 @@ from . import ProtectedResource
 from .emit import GrampsJSONEncoder
 
 
-class TranslateResource(ProtectedResource, GrampsJSONEncoder):
+class TranslationResource(ProtectedResource, GrampsJSONEncoder):
     """Translation resource."""
 
     @use_args(
-        {"strings": fields.Str(), "lang": fields.Str()},
+        {"strings": fields.Str(required=True)},
         location="query",
     )
-    def get(self, args: Dict) -> Response:
+    def get(self, args: Dict, lang: str) -> Response:
         """Get translation."""
-        if "strings" not in args:
-            return self.response(GRAMPS_LOCALE.get_language_dict())
-
         try:
             strings = json.loads(args["strings"])
         except json.JSONDecodeError:
             abort(400)
-        if args.get("lang"):
-            gramps_locale = GrampsLocale(lang=args["lang"])
-        else:
-            gramps_locale = GRAMPS_LOCALE
+
+        gramps_locale = GrampsLocale(lang=lang)
         return self.response(
             {s: gramps_locale.translation.sgettext(s) for s in strings}
         )
+
+
+class TranslationsResource(ProtectedResource, GrampsJSONEncoder):
+    """Translations resource."""
+
+    def get(self) -> Response:
+        """Get available translations."""
+        return self.response(GRAMPS_LOCALE.get_language_dict())
