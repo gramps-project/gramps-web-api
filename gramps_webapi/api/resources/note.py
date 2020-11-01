4b2a440371d609c63d14a4bf9c6a08fe8ee2a864
David Straub <straub@protonmail.com>
2020-11-01 16:46:30 +0100

    Implement HTML note formatter

--- gramps_webapi/api/resources/note.py
+++ gramps_webapi/api/resources/note.py
@@ -1,16 +1,45 @@
 """Note API resource."""
 
+from typing import Dict
+
+from gramps.gen.lib import Note
+
 from .base import (
     GrampsObjectProtectedResource,
     GrampsObjectResourceHelper,
     GrampsObjectsProtectedResource,
 )
 
+from ..html import get_note_html
+
 
 class NoteResourceHelper(GrampsObjectResourceHelper):
     """Note resource helper."""
 
     gramps_class_name = "Note"
+
+    # supported formatted note formats (all lowercase!)
+    FORMATS_SUPPORTED = ["html"]
+
+    def object_extend(self, obj: Note, args: Dict) -> Note:
+        """Extend note attributes as needed."""
+        if "formats" in args:
+            formats_allowed = [
+                fmt.lower()
+                for fmt in args["formats"]
+                if fmt.lower() in set(self.FORMATS_SUPPORTED)
+            ]
+            obj.formatted = {
+                fmt: self.get_formatted_note(note=obj, fmt=fmt)
+                for fmt in formats_allowed
+            }
+        return obj
+
+    def get_formatted_note(self, note: Note, fmt: str) -> str:
+        """Get the note text in a specific format."""
+        if fmt.lower() == "html":
+            return get_note_html(note)
+        raise ValueError("Format {} not known or supported.".format(fmt))
 
 
 class NoteResource(GrampsObjectProtectedResource, NoteResourceHelper):
