e93e5ca12c1a9d1b8b61897fd3facbc67c2ddb52
Christopher Horn <cdhorn@embarqmail.com>
2020-11-09 11:48:21 -0500

    Refactor /api/relations and add unit tests

--- gramps_webapi/api/resources/relation.py
+++ gramps_webapi/api/resources/relation.py
@@ -3,6 +3,7 @@
 from typing import Dict
 
 from flask import Response, abort
+from gramps.gen.const import GRAMPS_LOCALE
 from gramps.gen.db.base import DbReadBase
 from gramps.gen.relationship import RelationshipCalculator
 from webargs import fields
@@ -24,39 +25,32 @@ class RelationResource(ProtectedResource, GrampsJSONEncoder):
         return get_dbstate().db
 
     @use_args(
-        {"depth": fields.Integer(), "all": fields.Boolean()},
+        {"depth": fields.Integer(), "locale": fields.Boolean(missing=False)},
         location="query",
     )
     def get(self, args: Dict, handle1: Handle, handle2: Handle) -> Response:
-        """Get the relationship between two people."""
+        """Get the most direct relationship between two people."""
         db_handle = self.db_handle
         person1 = get_person_by_handle(db_handle, handle1)
-        if person1 is None:
+        if person1 == {}:
             abort(404)
 
         person2 = get_person_by_handle(db_handle, handle2)
-        if person2 is None:
+        if person2 == {}:
             abort(404)
 
         calc = RelationshipCalculator()
         if "depth" in args:
             calc.set_depth(args["depth"])
 
-        if "all" in args and args["all"]:
-            data = calc.get_all_relationships(db_handle, person1, person2)
-            index = 0
-            result = []
-            while index < len(data[0]):
-                result.append(
-                    {
-                        "relationship_string": data[0][index],
-                        "common_ancestors": data[1][index],
-                    }
-                )
-                index = index + 1
-            return self.response(200, result)
-
-        data = calc.get_one_relationship(db_handle, person1, person2, extra_info=True)
+        if args["locale"]:
+            data = calc.get_one_relationship(
+                db_handle, person1, person2, extra_info=True, olocale=GRAMPS_LOCALE
+            )
+        else:
+            data = calc.get_one_relationship(
+                db_handle, person1, person2, extra_info=True
+            )
         return self.response(
             200,
             {
@@ -65,3 +59,48 @@ class RelationResource(ProtectedResource, GrampsJSONEncoder):
                 "distance_common_other": data[2],
             },
         )
+
+
+class RelationsResource(ProtectedResource, GrampsJSONEncoder):
+    """Relations resource."""
+
+    @property
+    def db_handle(self) -> DbReadBase:
+        """Get the database instance."""
+        return get_dbstate().db
+
+    @use_args(
+        {
+            "depth": fields.Integer(),
+        },
+        location="query",
+    )
+    def get(self, args: Dict, handle1: Handle, handle2: Handle) -> Response:
+        """Get all possible relationships between two people."""
+        db_handle = self.db_handle
+        person1 = get_person_by_handle(db_handle, handle1)
+        if person1 == {}:
+            abort(404)
+
+        person2 = get_person_by_handle(db_handle, handle2)
+        if person2 == {}:
+            abort(404)
+
+        calc = RelationshipCalculator()
+        if "depth" in args:
+            calc.set_depth(args["depth"])
+
+        data = calc.get_all_relationships(db_handle, person1, person2)
+        result = []
+        index = 0
+        while index < len(data[0]):
+            result.append(
+                {
+                    "relationship_string": data[0][index],
+                    "common_ancestors": data[1][index],
+                }
+            )
+            index = index + 1
+        if result == []:
+            result = [{}]
+        return self.response(200, result)
