f8ccecc969598f74668bd9ce499c031a3d8cf1a9
Christopher Horn <cdhorn@embarqmail.com>
2020-11-14 07:26:27 -0500

    Refactor types into separate endpoint. (Closes #62) (#63)
    * Refactor types into separate endpoint.
    
    * Refactor /api/types for consistency with other endpoints
    
    * Factor out unneeded sibling/partner types

---
+++ gramps_webapi/api/resources/types.py
@@ -0,0 +1,244 @@
+"""Types API resource."""
+
+from typing import Dict, List, Optional
+
+from flask import Response, abort
+from gramps.gen.db.base import DbReadBase
+from gramps.gen.lib.attrtype import AttributeType
+from gramps.gen.lib.childreftype import ChildRefType
+from gramps.gen.lib.eventroletype import EventRoleType
+from gramps.gen.lib.eventtype import EventType
+from gramps.gen.lib.familyreltype import FamilyRelType
+from gramps.gen.lib.nameorigintype import NameOriginType
+from gramps.gen.lib.nametype import NameType
+from gramps.gen.lib.notetype import NoteType
+from gramps.gen.lib.person import Person
+from gramps.gen.lib.placetype import PlaceType
+from gramps.gen.lib.repotype import RepositoryType
+from gramps.gen.lib.srcattrtype import SrcAttributeType
+from gramps.gen.lib.srcmediatype import SourceMediaType
+from gramps.gen.lib.urltype import UrlType
+from webargs import fields
+from webargs.flaskparser import use_args
+
+from ..util import get_dbstate
+from . import ProtectedResource
+from .emit import GrampsJSONEncoder
+
+_DEFAULT_TYPE_CLASSES = {
+    "attribute_types": AttributeType(),
+    "event_types": EventType(),
+    "event_role_types": EventRoleType(),
+    "child_reference_types": ChildRefType(),
+    "family_relation_types": FamilyRelType(),
+    "name_origin_types": NameOriginType(),
+    "name_types": NameType(),
+    "note_types": NoteType(),
+    "place_types": PlaceType(),
+    "repository_types": RepositoryType(),
+    "source_attribute_types": SrcAttributeType(),
+    "source_media_types": SourceMediaType(),
+    "url_types": UrlType(),
+}
+
+person = Person()
+
+_GENDER_TYPES = {
+    person.MALE: "Male",
+    person.FEMALE: "Female",
+    person.UNKNOWN: "Unknown",
+}
+
+_DEFAULT_RECORD_TYPES = [
+    "attribute_types",
+    "child_reference_types",
+    "event_role_types",
+    "event_types",
+    "family_relation_types",
+    "gender_types",
+    "name_origin_types",
+    "name_types",
+    "note_types",
+    "place_types",
+    "repository_types",
+    "source_attribute_types",
+    "source_media_types",
+    "url_types",
+]
+
+_CUSTOM_RECORD_TYPES = [
+    "child_reference_types",
+    "event_attribute_types",
+    "event_role_types",
+    "event_types",
+    "family_attribute_types",
+    "family_relation_types",
+    "media_attribute_types",
+    "name_origin_types",
+    "name_types",
+    "note_types",
+    "person_attribute_types",
+    "place_types",
+    "repository_types",
+    "source_attribute_types",
+    "source_media_types",
+    "url_types",
+]
+
+
+def get_default_types(datatype: str, locale: bool = False) -> Optional[List]:
+    """Return list of types for a default record type."""
+    result = None
+    if datatype in _DEFAULT_TYPE_CLASSES:
+        types = _DEFAULT_TYPE_CLASSES[datatype]
+        if locale:
+            result = types.get_standard_names()
+        else:
+            result = types.get_standard_xml()
+    elif datatype == "gender_types":
+        result = [_GENDER_TYPES[x] for x in _GENDER_TYPES]
+    return result
+
+
+def get_custom_types(db_handle: DbReadBase, datatype: str) -> Optional[List]:
+    """Return list of types for a custom record type."""
+    result = None
+    if datatype == "event_attribute_types":
+        result = db_handle.get_event_attribute_types()
+    elif datatype == "event_types":
+        result = db_handle.get_event_types()
+    elif datatype == "person_attribute_types":
+        result = db_handle.get_person_attribute_types()
+    elif datatype == "family_attribute_types":
+        result = db_handle.get_family_attribute_types()
+    elif datatype == "media_attribute_types":
+        result = db_handle.get_media_attribute_types()
+    elif datatype == "family_relation_types":
+        result = db_handle.get_family_relation_types()
+    elif datatype == "child_reference_types":
+        result = db_handle.get_child_reference_types()
+    elif datatype == "event_role_types":
+        result = db_handle.get_event_roles()
+    elif datatype == "name_types":
+        result = db_handle.get_name_types()
+    elif datatype == "name_origin_types":
+        result = db_handle.get_origin_types()
+    elif datatype == "repository_types":
+        result = db_handle.get_repository_types()
+    elif datatype == "note_types":
+        result = db_handle.get_note_types()
+    elif datatype == "source_attribute_types":
+        result = db_handle.get_source_attribute_types()
+    elif datatype == "source_media_types":
+        result = db_handle.get_source_media_types()
+    elif datatype == "url_types":
+        result = db_handle.get_url_types()
+    elif datatype == "place_types":
+        result = db_handle.get_place_types()
+    return result
+
+
+class DefaultTypesResource(ProtectedResource, GrampsJSONEncoder):
+    """Default types resource."""
+
+    @use_args(
+        {
+            "locale": fields.Boolean(missing=False),
+        },
+        location="query",
+    )
+    def get(self, args: Dict) -> Response:
+        """Return a list of available default types."""
+        result = {}
+        for datatype in _DEFAULT_RECORD_TYPES:
+            result.update({datatype: get_default_types(datatype, args["locale"])})
+        return self.response(200, result)
+
+
+class DefaultTypeResource(ProtectedResource, GrampsJSONEncoder):
+    """Default type resource."""
+
+    @use_args(
+        {
+            "locale": fields.Boolean(missing=False),
+        },
+        location="query",
+    )
+    def get(self, args: Dict, datatype: str) -> Response:
+        """Return a list of values for a default type."""
+        result = get_default_types(datatype, args["locale"])
+        if result is None:
+            abort(404)
+        return self.response(200, result)
+
+
+class DefaultTypeMapResource(ProtectedResource, GrampsJSONEncoder):
+    """Default type resource."""
+
+    def get(self, datatype: str) -> Response:
+        """Return the map for a default type."""
+        if datatype in _DEFAULT_TYPE_CLASSES:
+            types = _DEFAULT_TYPE_CLASSES[datatype]
+            result = types.get_map()
+        elif datatype == "gender_types":
+            result = _GENDER_TYPES
+        else:
+            abort(404)
+        return self.response(200, result)
+
+
+class CustomTypesResource(ProtectedResource, GrampsJSONEncoder):
+    """Custom types resource."""
+
+    @property
+    def db_handle(self) -> DbReadBase:
+        """Get the database instance."""
+        return get_dbstate().db
+
+    def get(self) -> Response:
+        """Return a list of available custom types."""
+        result = {}
+        for datatype in _CUSTOM_RECORD_TYPES:
+            result.update({datatype: get_custom_types(self.db_handle, datatype)})
+        return self.response(200, result)
+
+
+class CustomTypeResource(ProtectedResource, GrampsJSONEncoder):
+    """Custom type resource."""
+
+    @property
+    def db_handle(self) -> DbReadBase:
+        """Get the database instance."""
+        return get_dbstate().db
+
+    def get(self, datatype: str) -> Response:
+        """Return list of values for the custom type."""
+        result = get_custom_types(self.db_handle, datatype)
+        if result is None:
+            abort(404)
+        return self.response(200, result)
+
+
+class TypesResource(ProtectedResource, GrampsJSONEncoder):
+    """Types resource."""
+
+    @property
+    def db_handle(self) -> DbReadBase:
+        """Get the database instance."""
+        return get_dbstate().db
+
+    @use_args(
+        {
+            "locale": fields.Boolean(missing=False),
+        },
+        location="query",
+    )
+    def get(self, args: Dict) -> Response:
+        """Return list of values for the custom type."""
+        custom = {}
+        for datatype in _CUSTOM_RECORD_TYPES:
+            custom.update({datatype: get_custom_types(self.db_handle, datatype)})
+        default = {}
+        for datatype in _DEFAULT_RECORD_TYPES:
+            default.update({datatype: get_default_types(datatype, args["locale"])})
+        return self.response(200, {"default": default, "custom": custom})
---
+++ tests/test_endpoints/test_types.py
@@ -0,0 +1,114 @@
+"""Tests for the /api/types endpoints using example_gramps."""
+
+import unittest
+
+from jsonschema import RefResolver, validate
+
+from . import API_SCHEMA, get_test_client
+
+
+class TestTypes(unittest.TestCase):
+    """Test cases for the /api/types endpoint."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Test class setup."""
+        cls.client = get_test_client()
+
+    def test_types_endpoint(self):
+        """Test response for types listing."""
+        # check expected number of type classes found
+        result = self.client.get("/api/types/")
+        self.assertEqual(len(result.json), 2)
+        # check response conforms to schema
+        resolver = RefResolver(base_uri="", referrer=API_SCHEMA, store={"": API_SCHEMA})
+        validate(
+            instance=result.json["default"],
+            schema=API_SCHEMA["definitions"]["DefaultTypes"],
+            resolver=resolver,
+        )
+        validate(
+            instance=result.json["custom"],
+            schema=API_SCHEMA["definitions"]["CustomTypes"],
+            resolver=resolver,
+        )
+
+
+class TestDefaultTypes(unittest.TestCase):
+    """Test cases for the /api/types/default endpoints."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Test class setup."""
+        cls.client = get_test_client()
+
+    def test_default_types_endpoint(self):
+        """Test response for default types listing."""
+        # check expected number of record types found
+        result = self.client.get("/api/types/default/")
+        self.assertGreaterEqual(len(result.json), 14)
+        # check response conforms to schema
+        resolver = RefResolver(base_uri="", referrer=API_SCHEMA, store={"": API_SCHEMA})
+        validate(
+            instance=result.json,
+            schema=API_SCHEMA["definitions"]["DefaultTypes"],
+            resolver=resolver,
+        )
+
+    def test_default_types_type_endpoint(self):
+        """Test response for default types type listing."""
+        # check valid response for each type in listing
+        type_list = self.client.get("/api/types/default/")
+        for item in type_list.json:
+            result = self.client.get("/api/types/default/" + item)
+            self.assertEqual(result.status_code, 200)
+        # check 404 for invalid type
+        result = self.client.get("/api/types/default/junk")
+        self.assertEqual(result.status_code, 404)
+
+    def test_default_types_type_map_endpoint(self):
+        """Test response for default types type map listing."""
+        # check valid response for each type in listing
+        type_list = self.client.get("/api/types/default/")
+        for item in type_list.json:
+            result = self.client.get("/api/types/default/" + item + "/map")
+            self.assertEqual(result.status_code, 200)
+            self.assertIsInstance(result.json, type({}))
+        # check 404 for invalid path
+        result = self.client.get("/api/types/default/junk/map")
+        self.assertEqual(result.status_code, 404)
+        result = self.client.get("/api/types/default/event_type/junk")
+        self.assertEqual(result.status_code, 404)
+
+
+class TestCustomTypes(unittest.TestCase):
+    """Test cases for the /api/types/custom endpoints."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Test class setup."""
+        cls.client = get_test_client()
+
+    def test_custom_types_endpoint(self):
+        """Test response for custom types listing."""
+        # check expected number of types found
+        result = self.client.get("/api/types/custom/")
+        self.assertGreaterEqual(len(result.json), 16)
+        # check response conforms to schema
+        resolver = RefResolver(base_uri="", referrer=API_SCHEMA, store={"": API_SCHEMA})
+        validate(
+            instance=result.json,
+            schema=API_SCHEMA["definitions"]["CustomTypes"],
+            resolver=resolver,
+        )
+
+    def test_custom_types_type_endpoint(self):
+        """Test response for custom types type listing."""
+        # check valid response for each type in listing
+        type_list = self.client.get("/api/types/custom/")
+        for item in type_list.json:
+            result = self.client.get("/api/types/custom/" + item)
+            self.assertEqual(result.status_code, 200)
+        # check 404 for invalid type
+        result = self.client.get("/api/types/custom/junk")
+        self.assertEqual(result.status_code, 404)
