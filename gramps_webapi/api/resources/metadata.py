ed1762bdd5786efacf86d5e478093d281536d4cf
Christopher Horn <cdhorn@embarqmail.com>
2020-11-15 17:39:59 -0500

    Remove exposure of path and config options

--- gramps_webapi/api/resources/metadata.py
+++ gramps_webapi/api/resources/metadata.py
@@ -1,11 +1,8 @@
 """Metadata API resource."""
 
-from typing import Dict
-
 import yaml
 from flask import Response
 from gramps.cli.clidbman import CLIDbManager
-from gramps.gen.config import get, get_section_settings, get_sections
 from gramps.gen.const import ENV, GRAMPS_LOCALE
 from gramps.gen.db.base import DbReadBase
 from pkg_resources import resource_filename
@@ -15,19 +12,6 @@ from gramps_webapi.const import VERSION
 from ..util import get_dbstate
 from . import ProtectedResource
 from .emit import GrampsJSONEncoder
-
-
-def get_config() -> Dict:
-    """Get the Gramps configuration options."""
-    config = {}
-    for section in get_sections():
-        data = {}
-        for setting in get_section_settings(section):
-            key = section + "." + setting
-            value = get(key)
-            data.update({key: value})
-        config.update({section: data})
-    return config
 
 
 class MetadataResource(ProtectedResource, GrampsJSONEncoder):
@@ -57,21 +41,19 @@ class MetadataResource(ProtectedResource, GrampsJSONEncoder):
             "database": {
                 "id": db_handle.get_dbid(),
                 "name": db_name,
-                "savepath": db_handle.get_save_path(),
                 "type": db_type,
             },
             "default_person": db_handle.get_default_handle(),
-            "gramps": {"config": get_config(), "env": ENV},
+            "gramps": {
+                "version": ENV["VERSION"],
+            },
             "gramps_webapi": {
                 "schema_version": schema["info"]["version"],
                 "version": VERSION,
             },
             "locale": {
                 "lang": GRAMPS_LOCALE.lang,
-                "localedir": GRAMPS_LOCALE.localedir,
-                "localedomain": GRAMPS_LOCALE.localedomain,
             },
-            "mediapath": db_handle.get_mediapath(),
             "object_counts": {},
             "researcher": db_handle.get_researcher(),
             "surnames": db_handle.get_surname_list(),
@@ -81,7 +63,8 @@ class MetadataResource(ProtectedResource, GrampsJSONEncoder):
             key = item.replace(" ", "_").lower()
             if "database" in key or "schema" in key:
                 key = key.replace("database_", "")
-                result["database"].update({key: data[item]})
+                if key != "module_location":
+                    result["database"].update({key: data[item]})
             elif "number_of" in key:
                 key = key.replace("number_of_", "")
                 result["object_counts"].update({key: data[item]})
