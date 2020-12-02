ce71a0a7f1cd180a5770798a09f31ec85f5326b9
David Straub <straub@protonmail.com>
2020-12-02 22:01:28 +0100

    Try to debug test

--- tests/test_endpoints/test_user.py
+++ tests/test_endpoints/test_user.py
@@ -147,7 +147,7 @@ class TestUser(unittest.TestCase):
             # extract the token from the message body
             body = msg.get_body().get_payload().replace("=\n", "")
             matches = re.findall(r".*jwt=3D([^\s]+).*", body)
-            self.assertEqual(len(matches), 1)
+            self.assertEqual(len(matches), 1, msg=body)
             token = matches[0]
         # try without token!
         rv = self.client.post(
