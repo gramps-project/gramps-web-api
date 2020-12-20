b2a8d1cec883b438ed375d8afce6996cba14b6cc
David Straub <straub@protonmail.com>
2020-12-20 15:58:16 +0100

    Add new database manager class (fixes #16) (#127)
    * Add new db manager class
    
    * Fix & extend test

--- gramps_webapi/dbmanager.py
+++ gramps_webapi/dbmanager.py
@@ -23,11 +23,12 @@
 import os
 
 from gramps.cli.clidbman import CLIDbManager
-from gramps.cli.grampscli import CLIManager
 from gramps.cli.user import User
-from gramps.gen.db.dbconst import DBLOCKFN
+from gramps.gen.db.dbconst import DBLOCKFN, DBMODE_R, DBMODE_W
 from gramps.gen.db.utils import get_dbid_from_path
 from gramps.gen.dbstate import DbState
+
+from .dbloader import WebDbSessionManager
 
 
 class WebDbManager:
@@ -71,16 +72,18 @@ class WebDbManager:
         if os.path.exists(os.path.join(self.path, DBLOCKFN)):
             os.unlink(os.path.join(self.path, DBLOCKFN))
 
-    def get_db(self, force_unlock: bool = False) -> DbState:
+    def get_db(self, lock: bool = False, force_unlock: bool = False) -> DbState:
         """Open the database and return a dbstate instance.
 
+        If `lock` is `False`, will not write a lock file (use with care!).
         If `force_unlock` is `True`, will break an existing lock (use with care!).
         """
         dbstate = DbState()
         user = User()
-        smgr = CLIManager(dbstate, True, user)
+        smgr = WebDbSessionManager(dbstate, user)
         smgr.do_reg_plugins(dbstate, uistate=None)
         if force_unlock:
             self.break_lock()
-        smgr.open_activate(self.path)
+        mode = DBMODE_W if lock else DBMODE_R
+        smgr.open_activate(self.path, mode=mode)
         return dbstate
