bb29e41e4ea02f62720b71184413ef3c5f4c1241
Christopher Horn <cdhorn@embarqmail.com>
2020-12-20 10:33:27 -0500

    Extend strip support to any dictionary

--- gramps_webapi/api/resources/emit.py
+++ gramps_webapi/api/resources/emit.py
@@ -88,19 +88,18 @@ class GrampsJSONEncoder:
             mimetype="application/json",
         )
 
+    def is_null(self, value: Any) -> bool:
+        """Test for empty value."""
+        if value is None:
+            return True
+        try:
+            return len(value) == 0
+        except TypeError:
+            pass
+        return False
+
     def extract_object(self, obj: Any, apply_filter=True) -> Dict:
         """Extract and filter attributes for a Gramps object."""
-
-        def is_null(value: Any) -> bool:
-            """Test for empty value."""
-            if value is None:
-                return True
-            try:
-                return len(value) == 0
-            except TypeError:
-                pass
-            return False
-
         data = {}
         for key, value in obj.__class__.__dict__.items():
             if isinstance(value, property):
@@ -112,7 +111,7 @@ class GrampsJSONEncoder:
                     if self.filter_skip_keys and key in self.filter_skip_keys:
                         continue
                 value = getattr(obj, key)
-                if not self.strip_empty_keys or not is_null(value):
+                if not self.strip_empty_keys or not self.is_null(value):
                     data[key] = value
         for key, value in obj.__dict__.items():
             if key.startswith("_"):
@@ -129,7 +128,7 @@ class GrampsJSONEncoder:
                 value = []
             if key in ["mother_handle", "father_handle", "famc"] and value is None:
                 value = ""
-            if not self.strip_empty_keys or not is_null(value):
+            if not self.strip_empty_keys or not self.is_null(value):
                 data[key] = value
         return data
 
@@ -167,6 +166,7 @@ class GrampsJSONEncoder:
                         continue
                     if self.filter_skip_keys and key in self.filter_skip_keys:
                         continue
-                result.update({key: self.extract_objects(obj[key], level=level)})
+                if not self.strip_empty_keys or not self.is_null(obj[key]):
+                    result.update({key: self.extract_objects(obj[key], level=level)})
             return result
         return obj
