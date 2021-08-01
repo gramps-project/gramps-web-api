81bce694fc340a712568dfad63278581d5d884a4
David Straub <straub@protonmail.com>
2021-08-01 09:55:13 +0200

    Add license header

--- gramps_webapi/api/resources/delete.py
+++ gramps_webapi/api/resources/delete.py
@@ -1,3 +1,30 @@
+#
+# Gramps Web API - A RESTful API for the Gramps genealogy program
+#
+# Copyright (C) 2000-2007  Donald N. Allingham
+# Copyright (C) 2009       Brian G. Matherly
+# Copyright (C) 2009       Gary Burton
+# Copyright (C) 2009       Benny Malengier
+# Copyright (C) 2011       Tim G L Lyons
+# Copyright (C) 2010,2011,2014  Nick Hall
+# Copyright (C) 2021       David Straub
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+
 """Functions for deleting objects with references."""
 
 from typing import Any, Dict, List, Optional
