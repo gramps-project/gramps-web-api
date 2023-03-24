eb7a2409a599a52d04fc7d2619a4f5e5159b395a
David Straub <straub@protonmail.com>
2023-03-24 08:12:03 +0100

    Refactor S3 media handler (#344)
    * Refactor S3
    
    * Fixes and test
    
    * Add requirement
    
    * Fix workflow
    
    * Remove s3 key cache

---
+++ tests/test_s3.py
@@ -0,0 +1,61 @@
+#
+# Gramps Web API - A RESTful API for the Gramps genealogy program
+#
+# Copyright (C) 2023      David Straub
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""Tests for the file and thumbnail endpoints using example_gramps."""
+
+import os
+import unittest
+from unittest.mock import patch
+
+import boto3
+import pytest
+from moto import mock_s3
+
+from gramps_webapi.api.media import MediaHandler
+from .test_endpoints.test_upload import get_image
+
+
+BUCKET = "test-s3-bucket"
+URL = f"s3://{BUCKET}"
+
+
+@pytest.fixture
+def bucket():
+    with mock_s3():
+        res = boto3.resource("s3", region_name="us-east-1")
+        res.create_bucket(Bucket=BUCKET)
+        yield
+
+
+def test_mediahandler(bucket):
+    handler = MediaHandler(URL)
+    assert handler.endpoint_url is None
+    assert handler.bucket_name == BUCKET
+    assert handler.remote_keys == set()
+
+
+def test_upload(bucket):
+    handler = MediaHandler(URL)
+    img, checksum = get_image(0)
+    assert handler.remote_keys == set()
+    handler.upload_file(img, checksum, "image/jpeg")
+    assert handler.remote_keys == {checksum}
+    img, checksum2 = get_image(1)
+    handler.upload_file(img, checksum2, "image/jpeg")
+    assert handler.remote_keys == {checksum, checksum2}
