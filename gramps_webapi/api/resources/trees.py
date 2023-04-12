87d336b392d7b1970a6d6900c8ec06b23ce14529
David Straub <straub@protonmail.com>
2023-04-12 22:57:24 +0200

    Start implementing tree endpoint

---
+++ gramps_webapi/api/resources/trees.py
@@ -0,0 +1,125 @@
+#
+# Gramps Web API - A RESTful API for the Gramps genealogy program
+#
+# Copyright (C) 2023      David Straub
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""Background task resources."""
+
+import os
+import re
+from typing import Dict, Optional
+
+from flask import abort
+from gramps.gen.config import config
+from webargs import fields
+from werkzeug.security import safe_join
+
+from ...auth.const import PERM_ADD_TREE, PERM_EDIT_OTHER_TREE, PERM_EDIT_TREE
+from ...dbmanager import WebDbManager
+from ..auth import require_permissions
+from ..util import get_tree_from_jwt, use_args
+from . import ProtectedResource
+
+# legal tree dirnames
+TREE_ID_REGEX = re.compile(r"^[a-zA-Z0-9_-]+$")
+
+
+def get_tree_details(tree_id: str) -> Dict[str, str]:
+    """Get details about a tree."""
+    try:
+        dbmgr = WebDbManager(dirname=tree_id, create_if_missing=False)
+    except ValueError:
+        abort(404)
+    return {"name": dbmgr.name, "id": tree_id}
+
+
+def get_tree_path(tree_id: str) -> Optional[str]:
+    """Get the path to the tree."""
+    dbdir = config.get("database.path")
+    return safe_join(dbdir, tree_id)
+
+
+def tree_exists(tree_id: str) -> bool:
+    """Check whether a tree exists."""
+    tree_path = get_tree_path(tree_id)
+    return tree_path and os.path.isdir(tree_path)
+
+
+def validate_tree_id(tree_id: str) -> None:
+    """Raise an error if the tree ID has an illegal format."""
+    if not TREE_ID_REGEX.match(tree_id):
+        abort(422)
+
+
+def get_single_tree_id() -> str:
+    """Get the tree ID in the case of using app.config['TREE']."""
+    dbmgr = WebDbManager(name=current_app.config["TREE"], create_if_missing=False)
+    return dbmgr.dirname
+
+
+class TreesResource(ProtectedResource):
+    """Resource for getting info about trees."""
+
+    def get(self):
+        """Get info about all trees."""
+        user_tree_id = get_tree_from_jwt() or get_single_tree_id()
+        # only allowed to see details about our own tree
+        tree_ids = [user_tree_id]
+        return [get_tree_details(tree_id) for tree_id in tree_ids]
+
+
+class TreeResource(ProtectedResource):
+    """Resource for a single tree."""
+
+    def get(self, tree_id: str):
+        """Get info about a tree."""
+        validate_tree_id(tree_id)
+        user_tree_id = get_tree_from_jwt() or get_single_tree_id()
+        if tree_id != user_tree_id:
+            # only allowed to see details about our own tree
+            abort(403)
+        return get_tree_details(tree_id)
+
+    def put(self, tree_id: str):
+        """Modify a tree."""
+        user_tree_id = get_tree_from_jwt() or get_single_tree_id()
+        if tree_id == user_tree_id:
+            require_permissions([PERM_EDIT_TREE])
+        else:
+            require_permissions([PERM_EDIT_OTHER_TREE])
+            validate_tree_id(tree_id)
+        try:
+            dbmgr = WebDbManager(dirname=tree_id, create_if_missing=False)
+        except ValueError:
+            abort(404)
+        # TODO
+        return "", 200
+
+    @use_args(
+        {
+            "name": fields.Str(required=True),
+        },
+        location="json",
+    )
+    def post(self, args, tree_id: str):
+        """Create a new tree."""
+        require_permissions([PERM_ADD_TREE])
+        validate_tree_id(tree_id)
+        if tree_exists(tree_id):
+            abort(409)
+        WebDbManager(dirname=tree_id, name=args["name"], create_if_missing=True)
+        return "", 201
---
+++ tests/test_endpoints/test_trees.py
@@ -0,0 +1,137 @@
+#
+# Gramps Web API - A RESTful API for the Gramps genealogy program
+#
+# Copyright (C) 2023      David Straub
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""Tests for the `gramps_webapi.api.resources.user` module."""
+
+import os
+import unittest
+from unittest.mock import patch
+
+from gramps.cli.clidbman import CLIDbManager
+from gramps.gen.dbstate import DbState
+
+from gramps_webapi.app import create_app
+from gramps_webapi.auth.const import ROLE_ADMIN, ROLE_OWNER
+from gramps_webapi.const import ENV_CONFIG_FILE, TEST_AUTH_CONFIG
+
+from . import BASE_URL
+
+
+class TestTrees(unittest.TestCase):
+    """Test cases for the /api/trees endpoints."""
+
+    def setUp(self):
+        self.name = "Test Web API"
+        self.dbman = CLIDbManager(DbState())
+        dbpath, _name = self.dbman.create_new_db_cli(self.name, dbid="sqlite")
+        self.tree = os.path.basename(dbpath)
+        with patch.dict("os.environ", {ENV_CONFIG_FILE: TEST_AUTH_CONFIG}):
+            self.app = create_app(config={"TESTING": True, "RATELIMIT_ENABLED": False})
+        self.client = self.app.test_client()
+        sqlauth = self.app.config["AUTH_PROVIDER"]
+        sqlauth.create_table()
+        sqlauth.add_user(
+            name="owner", password="123", email="owner@example.com", role=ROLE_OWNER
+        )
+        sqlauth.add_user(
+            name="admin", password="123", email="admin@example.com", role=ROLE_ADMIN
+        )
+        self.assertTrue(self.app.testing)
+        self.ctx = self.app.test_request_context()
+        self.ctx.push()
+
+    def tearDown(self):
+        self.ctx.pop()
+        self.dbman.remove_database(self.name)
+
+    def test_list_trees(self):
+        rv = self.client.post(
+            BASE_URL + "/token/", json={"username": "admin", "password": "123"}
+        )
+        token = rv.json["access_token"]
+        rv = self.client.get(
+            BASE_URL + "/trees/", headers={"Authorization": f"Bearer {token}"}
+        )
+        assert rv.status_code == 200
+        assert rv.json == [{"name": self.name, "id": self.tree}]
+
+    def test_get_tree(self):
+        rv = self.client.post(
+            BASE_URL + "/token/", json={"username": "admin", "password": "123"}
+        )
+        token = rv.json["access_token"]
+        rv = self.client.get(
+            BASE_URL + "/trees/..", headers={"Authorization": f"Bearer {token}"}
+        )
+        assert rv.status_code == 422
+        rv = self.client.get(
+            BASE_URL + "/trees/notexist", headers={"Authorization": f"Bearer {token}"}
+        )
+        assert rv.status_code == 403
+        rv = self.client.get(
+            BASE_URL + f"/trees/{self.tree}",
+            headers={"Authorization": f"Bearer {token}"},
+        )
+        assert rv.status_code == 200
+        assert rv.json == {"name": self.name, "id": self.tree}
+
+    def test_post_tree(self):
+        rv = self.client.post(
+            BASE_URL + "/token/", json={"username": "owner", "password": "123"}
+        )
+        token = rv.json["access_token"]
+        # wrong parameter name
+        rv = self.client.post(
+            BASE_URL + "/trees/id1",
+            headers={"Authorization": f"Bearer {token}"},
+            json={"value": "some name"},
+        )
+        assert rv.status_code == 422
+        # missing authorization
+        rv = self.client.post(
+            BASE_URL + "/trees/id1",
+            headers={"Authorization": f"Bearer {token}"},
+            json={"name": "some name"},
+        )
+        assert rv.status_code == 403
+        rv = self.client.post(
+            BASE_URL + "/token/", json={"username": "admin", "password": "123"}
+        )
+        token = rv.json["access_token"]
+        # exists
+        rv = self.client.post(
+            BASE_URL + f"/trees/{self.tree}",
+            headers={"Authorization": f"Bearer {token}"},
+            json={"name": "some name"},
+        )
+        assert rv.status_code == 409
+        # invalid
+        rv = self.client.post(
+            BASE_URL + "/trees/..",
+            headers={"Authorization": f"Bearer {token}"},
+            json={"name": "some name"},
+        )
+        assert rv.status_code == 422
+        # OK
+        rv = self.client.post(
+            BASE_URL + "/trees/newtree",
+            headers={"Authorization": f"Bearer {token}"},
+            json={"name": "some name"},
+        )
+        assert rv.status_code == 201
