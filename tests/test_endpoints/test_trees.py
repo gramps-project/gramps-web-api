630981c7d82f9b70c37e8e83595ff1216061b156
David Straub <straub@protonmail.com>
2023-04-18 20:14:08 +0200

    Fix test

--- tests/test_endpoints/test_trees.py
+++ tests/test_endpoints/test_trees.py
@@ -27,6 +27,7 @@ from gramps.cli.clidbman import CLIDbManager
 from gramps.gen.dbstate import DbState
 
 from gramps_webapi.app import create_app
+from gramps_webapi.auth import add_user, user_db
 from gramps_webapi.auth.const import ROLE_ADMIN, ROLE_OWNER
 from gramps_webapi.const import ENV_CONFIG_FILE, TEST_AUTH_CONFIG
 
@@ -44,14 +45,14 @@ class TestTrees(unittest.TestCase):
         with patch.dict("os.environ", {ENV_CONFIG_FILE: TEST_AUTH_CONFIG}):
             self.app = create_app(config={"TESTING": True, "RATELIMIT_ENABLED": False})
         self.client = self.app.test_client()
-        sqlauth = self.app.config["AUTH_PROVIDER"]
-        sqlauth.create_table()
-        sqlauth.add_user(
-            name="owner", password="123", email="owner@example.com", role=ROLE_OWNER
-        )
-        sqlauth.add_user(
-            name="admin", password="123", email="admin@example.com", role=ROLE_ADMIN
-        )
+        with self.app.app_context():
+            user_db.create_all()
+            add_user(
+                name="owner", password="123", email="owner@example.com", role=ROLE_OWNER
+            )
+            add_user(
+                name="admin", password="123", email="admin@example.com", role=ROLE_ADMIN
+            )
         self.assertTrue(self.app.testing)
         self.ctx = self.app.test_request_context()
         self.ctx.push()
