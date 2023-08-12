ab25d00922f7e3d50a1c6b0d4c3875c057ef41c1
David Straub <straub@protonmail.com>
2023-08-12 20:50:42 +0200

    Do not include surname list in metadata by default (#415)

--- gramps_webapi/api/resources/metadata.py
+++ gramps_webapi/api/resources/metadata.py
@@ -20,16 +20,17 @@
 
 """Metadata API resource."""
 
-from flask import current_app, Response
+from flask import Response, current_app
 from gramps.cli.clidbman import CLIDbManager
 from gramps.gen.const import ENV, GRAMPS_LOCALE
 from gramps.gen.db.base import DbReadBase
 from gramps.gen.dbstate import DbState
 from gramps.gen.utils.grampslocale import INCOMPLETE_TRANSLATIONS
+from webargs import fields
 
 from gramps_webapi.const import TREE_MULTI, VERSION
 
-from ..util import get_db_handle
+from ..util import get_db_handle, use_args
 from . import ProtectedResource
 from .emit import GrampsJSONEncoder
 
@@ -42,7 +43,13 @@ class MetadataResource(ProtectedResource, GrampsJSONEncoder):
         """Get the database instance."""
         return get_db_handle()
 
-    def get(self) -> Response:
+    @use_args(
+        {
+            "surnames": fields.Boolean(load_default=False),
+        },
+        location="query",
+    )
+    def get(self, args) -> Response:
         """Get active database and application related metadata information."""
         catalog = GRAMPS_LOCALE.get_language_dict()
         for entry in catalog:
@@ -100,8 +107,9 @@ class MetadataResource(ProtectedResource, GrampsJSONEncoder):
             },
             "researcher": db_handle.get_researcher(),
             "server": {"multi_tree": is_multi_tree, "task_queue": has_task_queue},
-            "surnames": db_handle.get_surname_list(),
         }
+        if args["surnames"]:
+            result["surnames"] = (db_handle.get_surname_list(),)
         data = db_handle.get_summary()
         db_version_key = GRAMPS_LOCALE.translation.sgettext("Database version")
         db_module_key = GRAMPS_LOCALE.translation.sgettext("Database module version")
