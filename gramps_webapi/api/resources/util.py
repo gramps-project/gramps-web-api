82e25d0b7adc5e5a3d50123959220de4648904bc
David Straub <straub@protonmail.com>
2023-08-19 11:45:21 +0200

    Version -> 1.2.1

--- gramps_webapi/api/resources/util.py
+++ gramps_webapi/api/resources/util.py
@@ -970,7 +970,11 @@ def _get_class_name(super_name, key_name) -> str:
         return "PlaceName"
     if key_name in ["primary_name", "alternate_names"]:
         return "Name"
-    if key_name == "attribute_list" and super_name == "Citation":
+    if (
+        key_name == "attribute_list"
+        and super_name == "Citation"
+        or super_name == "Source"
+    ):
         return "SrcAttribute"
     elif key_name == "attribute_list":
         return "Attribute"
