45e1552ed5dd91337a7f1472c38faa413140f69d
David Straub <straub@protonmail.com>
2024-01-30 18:54:14 +0100

    Add license note to check.py including Gramps contributors

--- gramps_webapi/api/check.py
+++ gramps_webapi/api/check.py
@@ -1,3 +1,26 @@
+#
+# Gramps - a GTK+/GNOME based genealogy program
+#
+# Copyright (C) 2000-2007  Donald N. Allingham
+# Copyright (C) 2008       Brian G. Matherly
+# Copyright (C) 2010       Jakim Friant
+# Copyright (C) 2011       Tim G L Lyons
+# Copyright (C) 2012       Michiel D. Nauta
+# Copyright (C) 2025       David Straub
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 """Check and repair a Gramps database."""
 
 from gramps.gen.db import DbTxn, DbWriteBase
