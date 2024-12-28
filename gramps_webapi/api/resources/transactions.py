cf1042d3c813655389c6b043114ac6628dc49063
David Straub <straub@protonmail.com>
2024-12-28 10:26:38 +0100

    Refactor transaction endpoint into standalone function

--- gramps_webapi/api/resources/transactions.py
+++ gramps_webapi/api/resources/transactions.py
@@ -1,7 +1,7 @@
 #
 # Gramps Web API - A RESTful API for the Gramps genealogy program
 #
-# Copyright (C) 2021-2023      David Straub
+# Copyright (C) 2021-2024      David Straub
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -60,6 +60,7 @@ class TransactionsResource(ProtectedResource):
         {
             "undo": fields.Boolean(load_default=False),
             "force": fields.Boolean(load_default=False),
+            "background": fields.Boolean(load_default=False),
         },
         location="query",
     )
@@ -72,72 +73,7 @@ class TransactionsResource(ProtectedResource):
         is_undo = args["undo"]
         if is_undo:
             payload = reverse_transaction(payload)
-        db_handle = get_db_handle(readonly=False)
-        num_people_deleted = sum(
-            item["type"] == "delete" and item["_class"] == "Person" for item in payload
-        )
-        num_people_added = sum(
-            item["type"] == "add" and item["_class"] == "Person" for item in payload
-        )
-        num_people_new = num_people_added - num_people_deleted
-        check_quota_people(to_add=num_people_new)
-        with DbTxn("Raw transaction", db_handle) as trans:
-            for item in payload:
-                try:
-                    class_name = item["_class"]
-                    trans_type = item["type"]
-                    handle = item["handle"]
-                    old_data = item["old"]
-                    if not args["force"] and not self.old_unchanged(
-                        db_handle, class_name, handle, old_data
-                    ):
-                        if num_people_added or num_people_deleted:
-                            update_usage_people()
-                        abort_with_message(409, "Object has changed")
-                    new_data = item["new"]
-                    if new_data:
-                        new_obj = from_json(json.dumps(new_data))
-                    if trans_type == "delete":
-                        self.handle_delete(trans, class_name, handle)
-                        if (
-                            class_name == "Person"
-                            and handle == db_handle.get_default_handle()
-                        ):
-                            db_handle.set_default_person_handle(None)
-                    elif trans_type == "add":
-                        self.handle_add(trans, class_name, new_obj)
-                    elif trans_type == "update":
-                        self.handle_commit(trans, class_name, new_obj)
-                    else:
-                        if num_people_added or num_people_deleted:
-                            update_usage_people()
-                        abort_with_message(400, "Unexpected transaction type")
-                except (KeyError, UnicodeDecodeError, json.JSONDecodeError, TypeError):
-                    if num_people_added or num_people_deleted:
-                        update_usage_people()
-                    abort_with_message(400, "Error while processing transaction")
-            trans_dict = transaction_to_json(trans)
-        if num_people_new:
-            update_usage_people()
-        # update search index
-        tree = get_tree_from_jwt_or_fail()
-        indexer: SearchIndexer = get_search_indexer(tree)
-        for _trans_dict in trans_dict:
-            handle = _trans_dict["handle"]
-            class_name = _trans_dict["_class"]
-            if _trans_dict["type"] == "delete":
-                indexer.delete_object(handle, class_name)
-            else:
-                indexer.add_or_update_object(handle, db_handle, class_name)
-        if app_has_semantic_search():
-            semantic_indexer: SemanticSearchIndexer = get_semantic_search_indexer(tree)
-            for _trans_dict in trans_dict:
-                handle = _trans_dict["handle"]
-                class_name = _trans_dict["_class"]
-                if _trans_dict["type"] == "delete":
-                    semantic_indexer.delete_object(handle, class_name)
-                else:
-                    semantic_indexer.add_or_update_object(handle, db_handle, class_name)
+        trans_dict = process_transactions(payload, force=args["force"])
         res = Response(
             response=json.dumps(trans_dict),
             status=200,
@@ -146,34 +82,108 @@ class TransactionsResource(ProtectedResource):
         res.headers.add("X-Total-Count", str(len(trans_dict)))
         return res
 
-    def handle_delete(self, trans: DbTxn, class_name: str, handle: str) -> None:
-        """Handle a delete action."""
-        del_func = trans.db.method("remove_%s", class_name)
-        del_func(handle, trans)
 
-    def handle_commit(self, trans: DbTxn, class_name: str, obj) -> None:
-        """Handle an update action."""
-        com_func = trans.db.method("commit_%s", class_name)
-        com_func(obj, trans)
+def process_transactions(payload, force: bool):
+    num_people_deleted = sum(
+        item["type"] == "delete" and item["_class"] == "Person" for item in payload
+    )
+    num_people_added = sum(
+        item["type"] == "add" and item["_class"] == "Person" for item in payload
+    )
+    num_people_new = num_people_added - num_people_deleted
+    check_quota_people(to_add=num_people_new)
+    db_handle = get_db_handle(readonly=False)
+    with DbTxn("Raw transaction", db_handle) as trans:
+        for item in payload:
+            try:
+                class_name = item["_class"]
+                trans_type = item["type"]
+                handle = item["handle"]
+                old_data = item["old"]
+                if not force and not old_unchanged(
+                    db_handle, class_name, handle, old_data
+                ):
+                    if num_people_added or num_people_deleted:
+                        update_usage_people()
+                    abort_with_message(409, "Object has changed")
+                new_data = item["new"]
+                if new_data:
+                    new_obj = from_json(json.dumps(new_data))
+                if trans_type == "delete":
+                    handle_delete(trans, class_name, handle)
+                    if (
+                        class_name == "Person"
+                        and handle == db_handle.get_default_handle()
+                    ):
+                        db_handle.set_default_person_handle(None)
+                elif trans_type == "add":
+                    handle_add(trans, class_name, new_obj)
+                elif trans_type == "update":
+                    handle_commit(trans, class_name, new_obj)
+                else:
+                    if num_people_added or num_people_deleted:
+                        update_usage_people()
+                    abort_with_message(400, "Unexpected transaction type")
+            except (KeyError, UnicodeDecodeError, json.JSONDecodeError, TypeError):
+                if num_people_added or num_people_deleted:
+                    update_usage_people()
+                abort_with_message(400, "Error while processing transaction")
+        trans_dict = transaction_to_json(trans)
+    if num_people_new:
+        update_usage_people()
+    # update search index
+    tree = get_tree_from_jwt_or_fail()
+    indexer: SearchIndexer = get_search_indexer(tree)
+    for _trans_dict in trans_dict:
+        handle = _trans_dict["handle"]
+        class_name = _trans_dict["_class"]
+        if _trans_dict["type"] == "delete":
+            indexer.delete_object(handle, class_name)
+        else:
+            indexer.add_or_update_object(handle, db_handle, class_name)
+    # update semantic search index
+    if app_has_semantic_search():
+        semantic_indexer: SemanticSearchIndexer = get_semantic_search_indexer(tree)
+        for _trans_dict in trans_dict:
+            handle = _trans_dict["handle"]
+            class_name = _trans_dict["_class"]
+            if _trans_dict["type"] == "delete":
+                semantic_indexer.delete_object(handle, class_name)
+            else:
+                semantic_indexer.add_or_update_object(handle, db_handle, class_name)
+    return trans_dict
 
-    def handle_add(self, trans: DbTxn, class_name: str, obj) -> None:
-        """Handle an add action."""
-        if class_name != "Tag" and not obj.gramps_id:
-            abort_with_message(400, "Gramps ID missing")
-        self.handle_commit(trans, class_name, obj)
 
-    def old_unchanged(
-        self, db: DbReadBase, class_name: str, handle: str, old_data: Dict
-    ) -> bool:
-        """Check if the "old" object is still unchanged."""
-        handle_func = db.method("get_%s_from_handle", class_name)
-        try:
-            obj = handle_func(handle)
-        except HandleError:
-            if old_data is None:
-                return True
-            return False
-        obj_dict = json.loads(to_json(obj))
-        if diff_items(class_name, old_data, obj_dict):
-            return False
-        return True
+def handle_delete(trans: DbTxn, class_name: str, handle: str) -> None:
+    """Handle a delete action."""
+    del_func = trans.db.method("remove_%s", class_name)
+    del_func(handle, trans)
+
+
+def handle_commit(trans: DbTxn, class_name: str, obj) -> None:
+    """Handle an update action."""
+    com_func = trans.db.method("commit_%s", class_name)
+    com_func(obj, trans)
+
+
+def handle_add(trans: DbTxn, class_name: str, obj) -> None:
+    """Handle an add action."""
+    if class_name != "Tag" and not obj.gramps_id:
+        abort_with_message(400, "Gramps ID missing")
+    handle_commit(trans, class_name, obj)
+
+
+def old_unchanged(db: DbReadBase, class_name: str, handle: str, old_data: Dict) -> bool:
+    """Check if the "old" object is still unchanged."""
+    handle_func = db.method("get_%s_from_handle", class_name)
+    assert handle_func is not None, "No handle function found"
+    try:
+        obj = handle_func(handle)
+    except HandleError:
+        if old_data is None:
+            return True
+        return False
+    obj_dict = json.loads(to_json(obj))
+    if diff_items(class_name, old_data, obj_dict):
+        return False
+    return True
