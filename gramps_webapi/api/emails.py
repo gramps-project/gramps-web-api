41f1c8d8becfdf5b97f1191bb19b14fb82ae98bb
Daniel Morsch <d.morsch.bs@gmail.com>
2025-02-08 13:04:30 +0100

    restruct email building and adapt unstyled mails

--- gramps_webapi/api/emails.py
+++ gramps_webapi/api/emails.py
@@ -21,86 +21,6 @@
 
 from gettext import gettext as _
 
-
-def email_reset_pw(base_url: str, user_name: str, token: str):
-    """Reset password e-mail text."""
-    intro = _(
-        "You are receiving this e-mail because you (or someone else) "
-        "have requested the reset of the password for your account."
-    )
-
-    action = _(
-        "Please click on the following link, or paste this into your browser "
-        "to complete the process:"
-    )
-
-    end = _(
-        "If you did not request this, please ignore this e-mail "
-        "and your password will remain unchanged."
-    )
-
-    url = f"""{base_url}/api/users/-/password/reset/?jwt={token}"""
-    body = f"""{intro}
-
-{action}
-
-{url}
-
-{end}
-"""
-
-    body_html = email_htmlbody_reset_pw(user_name=user_name, url=url)
-    return (body, body_html)
-
-
-def email_confirm_email(base_url: str, user_name: str, token: str):
-    """Confirm e-mail address e-mail text."""
-    intro = (
-        _(
-            "You are receiving this message because this e-mail address "
-            "was used to register a new account at %s."
-        )
-        % base_url
-    )
-    action = _(
-        "Please click on the following link, or paste this into your browser "
-        "to confirm your email address. You will be able to log on once a "
-        "tree owner reviews and approves your account."
-    )
-
-    url = f"""{base_url}/api/users/-/email/confirm/?jwt={token}"""
-
-    body = f"""{intro}
-
-{action}
-
-{url}
-"""
-    body_html = email_htmlbody_confirm_email(user_name=user_name, url=url)
-    return (body, body_html)
-
-
-def email_new_user(base_url: str, username: str, fullname: str, email: str):
-    """E-mail notifying owners of a new registered user."""
-    intro = _("A new user registered at %s.") % base_url
-    next_step = _(
-        "Please review this user registration and assign a role to " "enable access:"
-    )
-    label_username = _("User name")
-    label_fullname = _("Full name")
-    label_email = _("E-mail")
-    user_details = f"""{label_username}: {username}
-{label_fullname}: {fullname}
-{label_email}: {email}
-"""
-    return f"""{intro}
-
-{next_step}
-
-{user_details}
-"""
-
-
 EMAIL_CSS_STYLES = """
 body {
     font-family: Arial, sans-serif;
@@ -141,6 +61,94 @@ body {
 """
 
 
+def email_reset_pw(base_url: str, user_name: str, token: str):
+    """Reset password e-mail text."""
+    url = f"""{base_url}/api/users/-/password/reset/?jwt={token}"""
+
+    body = email_body_reset_pw(user_name=user_name, url=url)
+    body_html = email_htmlbody_reset_pw(user_name=user_name, url=url)
+    return (body, body_html)
+
+
+def email_confirm_email(base_url: str, user_name: str, token: str):
+    """Confirm e-mail address e-mail text."""
+    url = f"""{base_url}/api/users/-/email/confirm/?jwt={token}"""
+
+    body = email_body_confirm_email(user_name=user_name, url=url)
+    body_html = email_htmlbody_confirm_email(user_name=user_name, url=url)
+    return (body, body_html)
+
+
+def email_new_user(base_url: str, username: str, fullname: str, email: str):
+    """E-mail notifying owners of a new registered user."""
+    intro = _("A new user registered at %s.") % base_url
+    next_step = _(
+        "Please review this user registration and assign a role to " "enable access:"
+    )
+    label_username = _("User name")
+    label_fullname = _("Full name")
+    label_email = _("E-mail")
+    user_details = f"""{label_username}: {username}
+{label_fullname}: {fullname}
+{label_email}: {email}
+"""
+    return f"""{intro}
+
+{next_step}
+
+{user_details}
+"""
+
+
+def email_htmltemplate(header: str, containerContent: str) -> str:
+    return """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>%(header)s</title>
+            <style>
+                %(styles)s
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                %(containerContent)s
+            </div>
+        </body>
+        </html>
+    """ % {
+        "header": header,
+        "containerContent": containerContent,
+        "styles": EMAIL_CSS_STYLES,
+    }
+
+
+def email_body_reset_pw(user_name: str, url: str) -> str:
+    header = _("Reset Your Password")
+    greeting = _("Hi %s,") % user_name
+    descMail = _(
+        "You are receiving this e-mail because you (or someone else) have requested the reset of the password for your account."
+    )
+    descLink = _(
+        "Please click on the following link, or paste this into your browser to complete the process:"
+    )
+    descIgnore = _(
+        "If you did not request a password reset, please ignore this email and your password will remain unchanged."
+    )
+    return f"""{header}
+
+{greeting}
+{descMail}
+{descLink}
+
+{url}
+
+{descIgnore}
+    """
+
+
 def email_htmlbody_reset_pw(user_name: str, url: str) -> str:
     header = _("Reset Your Password")
     greeting = _("Hi %s,") % user_name
@@ -152,19 +160,8 @@ def email_htmlbody_reset_pw(user_name: str, url: str) -> str:
     descIgnore = _(
         "If you did not request a password reset, please ignore this email and your password will remain unchanged."
     )
-    return """
-    <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>%(header)s</title>
-    <style>
-        %(styles)s
-    </style>
-</head>
-<body>
-    <div class="container">
+
+    containerContent = """
         <div class="header">%(header)s</div>
         <div class="content">
             <p class="greeting">%(greeting)s</p>
@@ -173,9 +170,6 @@ def email_htmlbody_reset_pw(user_name: str, url: str) -> str:
             <a href="%(url)s" class="button">%(buttonLabel)s</a>
             <p>%(descIgnore)s</p>
         </div>
-    </div>
-</body>
-</html>
     """ % {
         "greeting": greeting,
         "url": url,
@@ -184,8 +178,34 @@ def email_htmlbody_reset_pw(user_name: str, url: str) -> str:
         "descAction": descAction,
         "buttonLabel": buttonLabel,
         "descIgnore": descIgnore,
-        "styles": EMAIL_CSS_STYLES,
     }
+
+    return email_htmltemplate(header, containerContent)
+
+
+def email_body_confirm_email(user_name: str, url: str) -> str:
+    header = _("Confirm your e-mail address")
+    welcome = _("Welcome to Gramps Web")
+    greeting = _("Hi %s,") % user_name
+    descLink = _(
+        "Please click on the following link, or paste this into your browser "
+        "to confirm your email address. You will be able to log on once a "
+        "tree owner reviews and approves your account."
+    )
+    descFurtherAction = _(
+        "You will be able to log on once a tree owner reviews and approves your account."
+    )
+
+    return f"""{header}
+
+{welcome}
+{greeting}
+{descLink}
+
+{url}
+
+{descFurtherAction}
+    """
 
 
 def email_htmlbody_confirm_email(user_name: str, url: str) -> str:
@@ -200,19 +220,7 @@ def email_htmlbody_confirm_email(user_name: str, url: str) -> str:
         "You will be able to log on once a tree owner reviews and approves your account."
     )
 
-    return """
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>%(header)s</title>
-    <style>
-        %(styles)s
-    </style>
-</head>
-<body>
-    <div class="container">
+    containerContent = """
         <div class="header">%(welcome)s</div>
         <div class="content">
             <p class="greeting">%(greeting)s</p>
@@ -220,17 +228,12 @@ def email_htmlbody_confirm_email(user_name: str, url: str) -> str:
             <a href="%(url)s" class="button">%(buttonLabel)s</a>
             <p>%(descFurtherAction)s</p>
         </div>
-    </div>
-</body>
-</html>
     """ % {
-        "user_name": user_name,
         "url": url,
-        "header": header,
         "welcome": welcome,
         "greeting": greeting,
         "descAction": descAction,
         "buttonLabel": buttonLabel,
         "descFurtherAction": descFurtherAction,
-        "styles": EMAIL_CSS_STYLES,
     }
+    return email_htmltemplate(header, containerContent)
