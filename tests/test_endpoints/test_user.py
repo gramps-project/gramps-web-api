5ca7fac25261b6eba8f58daba931989a9be189ed
DanielMorsch <d.morsch.bs@gmail.com>
2025-02-11 13:05:41 +0100

    Update regex to read token from html mail

--- tests/test_endpoints/test_user.py
+++ tests/test_endpoints/test_user.py
@@ -240,7 +240,7 @@ class TestUser(unittest.TestCase):
             msg = args[0]
             # extract the token from the message body
             body = msg.get_body().get_payload().replace("=\n", "")
-            matches = re.findall(r".*jwt=([^\s]+).*", body)
+            matches = re.findall(r"jwt=3D([a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+)", body)
             self.assertEqual(len(matches), 1, msg=body)
             token = matches[0]
             if token[:2] == "3D":
@@ -637,7 +637,7 @@ class TestUser(unittest.TestCase):
             msg = args[0]
             # extract the token from the message body
             body = msg.get_body().get_payload().replace("=\n", "")
-            matches = re.findall(r".*jwt=([^\s]+).*", body)
+            matches = re.findall(r"jwt=3D([a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+)", body)
             self.assertEqual(len(matches), 1, msg=body)
             token = matches[0]
             if token[:2] == "3D":
