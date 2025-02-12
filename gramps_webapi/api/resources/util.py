a6ae41933f914d4037c2a5ff300d787f48f346d5
David Straub <straub@protonmail.com>
2025-02-12 20:54:29 +0100

    Fix json functions

--- gramps_webapi/api/resources/util.py
+++ gramps_webapi/api/resources/util.py
@@ -26,7 +26,7 @@ import json
 import os
 from hashlib import sha256
 from http import HTTPStatus
-from typing import Any, cast, Optional, Union, Literal
+from typing import Any, Literal, Optional, Union, cast
 
 import gramps
 import gramps.gen.lib
@@ -53,9 +53,10 @@ from gramps.gen.lib import (
     Source,
     Span,
 )
+
+# from gramps.gen.lib.serialize import to_json
+from gramps.gen.lib.json_utils import object_to_dict, object_to_string
 from gramps.gen.lib.primaryobj import BasicPrimaryObject as GrampsObject
-#from gramps.gen.lib.serialize import to_json
-from gramps.gen.lib.json_utils import string_to_dict, object_to_string
 from gramps.gen.plug import BasePluginManager
 from gramps.gen.relationship import get_relationship_calculator
 from gramps.gen.soundex import soundex
@@ -1124,16 +1125,16 @@ def transaction_to_json(transaction: DbTxn) -> TransactionJson:
         trans_dict = {TXNUPD: "update", TXNDEL: "delete", TXNADD: "add"}
         obj_cls = getattr(gramps.gen.lib, obj_cls_name)
         # In Gramps 6.0, the data repr is a string
-        #if old_data:
+        # if old_data:
         #    old_data = obj_cls().unserialize(old_data)
-        #if new_data:
+        # if new_data:
         #    new_data = obj_cls().unserialize(new_data)
         item = {
             "type": trans_dict[action],
             "handle": handle,
             "_class": obj_cls_name,
-            "old": string_to_dict(old_data), # json.loads(to_json(old_data)),
-            "new": string_to_dict(new_data), # json.loads(to_json(new_data)),
+            "old": object_to_dict(old_data),  # json.loads(to_json(old_data)),
+            "new": object_to_dict(new_data),  # json.loads(to_json(new_data)),
         }
         out.append(item)
     return out
@@ -1157,7 +1158,7 @@ def reverse_transaction(transaction_list: TransactionJson) -> TransactionJson:
 
 def hash_object(obj: GrampsObject) -> str:
     """Generate a SHA256 hash for a Gramps object's data."""
-    data = object_to_string(obj)  #to_json(obj).encode()
+    data = object_to_string(obj)  # to_json(obj).encode()
     return sha256(data).hexdigest()
 
 
