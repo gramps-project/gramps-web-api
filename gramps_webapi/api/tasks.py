9becdcfda7736c56dc29f79f2ed3b612fc7b1a5f
David Straub <straub@protonmail.com>
2025-10-04 11:31:01 +0000

    In single-tree setup, send owner notification also for empty tree ID

--- gramps_webapi/api/tasks.py
+++ gramps_webapi/api/tasks.py
@@ -121,7 +121,12 @@ def send_email_confirm_email(email: str, user_name: str, token: str):
 
 @shared_task()
 def send_email_new_user(
-    username: str, fullname: str, email: str, tree: str, include_admins: bool
+    username: str,
+    fullname: str,
+    email: str,
+    tree: str,
+    include_admins: bool,
+    include_treeless: bool,
 ):
     """Send an email to owners to notify of a new registered user."""
     base_url = get_config("BASE_URL").rstrip("/")
@@ -129,7 +134,9 @@ def send_email_new_user(
         base_url=base_url, username=username, fullname=fullname, email=email
     )
     subject = _("New registered user")
-    emails = get_owner_emails(tree=tree, include_admins=include_admins)
+    emails = get_owner_emails(
+        tree=tree, include_admins=include_admins, include_treeless=include_treeless
+    )
     if emails:
         send_email(subject=subject, body=body, to=emails)
 
