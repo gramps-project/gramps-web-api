cc25d857a4b92cc8871089aafb6066faf3d2b5ab
David Straub <straub@protonmail.com>
2026-04-24 16:23:12 +0200

    Celery: always track started, catch aborts (#818)
    * Celery: always track started, catch aborts
    
    * Comments
    
    * Fix test
    
    * Remove silly non-test

--- gramps_webapi/util/celery.py
+++ gramps_webapi/util/celery.py
@@ -2,6 +2,8 @@
 
 from celery import Task
 from celery import current_app as current_celery_app
+from celery.exceptions import Ignore
+from werkzeug.exceptions import HTTPException
 
 
 def create_celery(app):
@@ -9,6 +11,8 @@ def create_celery(app):
     celery = current_celery_app
     celery.conf.name = app.import_name
     celery.conf.update(app.config["CELERY_CONFIG"])
+    # Always track started state so task status is accurate regardless of user config.
+    celery.conf.task_track_started = True
 
     class ContextTask(Task):
         """Celery task which is aware of the flask app context."""
@@ -17,7 +21,21 @@ def create_celery(app):
             if self.request.called_directly:
                 return self.run(*args, **kwargs)
             with app.app_context():
-                return self.run(*args, **kwargs)
+                try:
+                    return self.run(*args, **kwargs)
+                except HTTPException as exc:
+                    # Utility functions like check_quota_people and run_import
+                    # use abort_with_message (a Flask/HTTP construct) for both
+                    # request handlers and Celery tasks. Preserve the standard
+                    # API error shape here so task consumers do not need
+                    # special-case parsing for background-task failures.
+                    # Ideally these utilities would raise a dedicated
+                    # TaskError instead of an HTTPException.
+                    self.update_state(
+                        state="FAILURE",
+                        meta={"error": {"code": exc.code, "message": exc.description}},
+                    )
+                    raise Ignore()
 
     celery.Task = ContextTask
     return celery
